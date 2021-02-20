ae21feb22db4b82a09b0ee06f609efe65218dddc
Velibor Markovic <veljko85@users.noreply.github.com>
2021-02-20 18:55:06 +0100

    Add files via upload

---
+++ babylon.js
@@ -0,0 +1,144 @@
+/// <reference path='C:\Users\Veljko\Desktop\veljko\javaScript\babylonJS\babylon.d.ts' />
+
+var canvas = document.getElementById("renderCanvas");
+        var engine = null;
+        var scene = null;
+        var sceneToRender = null;
+        var createDefaultEngine = function() { return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true,  disableWebGL2Support: false}); };
+        var createScene = function () {
+            var scene = new BABYLON.Scene(engine);
+            scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+
+            var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, new BABYLON.Vector3(0, 0, 0), scene);
+            camera.setPosition(new BABYLON.Vector3(0, 0, -1.5));
+            camera.attachControl(canvas, true);
+        
+            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, -1), scene);
+        
+            var urlList = [
+                "http://doc.babylonjs.com/",
+                "http://www.babylonjs.com/",
+                "https://github.com/BabylonJS/Babylon.js/",
+            ];
+        
+            var textures = [
+                "Yugoslav_1_dinar_1965.png",
+                "1_dinar_RSD_(1904-1915).png",
+                "Yugoslavia_1_dinar_1976_FAO.png"
+        
+            ];
+            //make change on click outside of canvas
+
+            let imgClass = document.getElementsByClassName('img');
+
+            for (let i = 0; i < imgClass.length; i++) {
+                imgClass[i].addEventListener("click", () =>{
+                    cylinder.material.diffuseTexture = new BABYLON.Texture(textures[i]);
+                })
+                
+            }
+            //*************************************************************** */
+
+        
+        
+            // this is function that is called on each cylinder pick
+            var t = 0;
+            var pickCylinder = function(meshEvent) {
+                var pickedMesh = meshEvent.meshUnderPointer; 
+                if(t < 2){t += 1}else{t=0};
+                
+                cylinder.material.diffuseTexture = new BABYLON.Texture(textures[t]);
+                //window.open(urlList[Number(pickedMesh.name)]);
+            }
+
+
+            // BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/BabylonJS/Assets/master/meshes/Alien/", "Alien.gltf").then((result) => {   
+            //     const alien = result.meshes[0];
+            //     alien.scaling = new BABYLON.Vector3(2, 2, 2);
+            //     alien.rotation.z = BABYLON.Tools.ToRadians(20);
+            //     alien.material = new BABYLON.StandardMaterial("alienMat");
+            //     //aliMat.diffuseColor = new BABYLON.Color3(0,1,0); //rgb color
+            //     alien.material.diffuseTexture = new BABYLON.Texture(textures[0]); //Place the material property of the ground
+            // });
+
+            // BABYLON.SceneLoader.ImportMesh("", "https://models.babylonjs.com/", "ufo.glb", scene, function (meshes) {          
+            //     scene.createDefaultCameraOrLight(true, true, true);
+            //     scene.createDefaultEnvironment();
+            //     const disc = result.meshes[0];
+            //     disc.material.diffuseTexture = new BABYLON.Texture(textures[0]);
+                
+            // });
+
+            // var cube = BABYLON.MeshBuilder.CreateBox('cube', {}, scene);
+            // cube.position = new BABYLON.Vector3(-1, 0, 0);
+
+            //create lines
+            // const points = [
+            //     new BABYLON.Vector3(1,0,0),
+            //     new BABYLON.Vector3(1,1,1),
+            //     new BABYLON.Vector3(1,1,0)
+            // ];
+            // const lines = BABYLON.MeshBuilder.CreateLines('lines', {
+            //     points,
+            // })
+
+            // cylinder generator with actionManagers on each
+
+            const cylMat = new BABYLON.StandardMaterial("cylMat");
+                
+
+                const faceUV2 = [];
+                faceUV2[0] = new BABYLON.Vector4(0.5, 0.0, 1, 1);
+                faceUV2[1] = new BABYLON.Vector4(0.5, 0.0, 1, 1);
+                faceUV2[2] = new BABYLON.Vector4(0, 0, 0.5, 1);
+                
+
+                var cylinder  = BABYLON.MeshBuilder.CreateCylinder(0, {radius: 0.5, height: 0.05, faceUV: faceUV2}, scene);
+                //cylinder.scaling = new BABYLON.Vector3(1, 1, 1);
+                cylinder.material = cylMat;
+                cylinder.rotation.x = Math.PI/2;
+                cylinder.addRotation(0, Math.PI, 0.7);
+        
+                //cylinder.material = new BABYLON.StandardMaterial("mat_", scene);
+                cylMat.diffuseTexture = new BABYLON.Texture("Yugoslav_1_dinar_1965.png")
+                // add actionManager on each cyl
+                cylinder.actionManager = new BABYLON.ActionManager(scene);
+                // register 'pickCylinder' as the handler function for cylinder picking action.
+                cylinder.actionManager.registerAction(
+                    new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, pickCylinder)
+                );
+        
+           // } 
+        
+        
+        
+        
+        return scene;
+        }
+                var engine;
+                var scene;
+                initFunction = async function() {               
+                    var asyncEngineCreation = async function() {
+                        try {
+                        return createDefaultEngine();
+                        } catch(e) {
+                        console.log("the available createEngine function failed. Creating the default engine instead");
+                        return createDefaultEngine();
+                        }
+                    }
+
+                    engine = await asyncEngineCreation();
+        if (!engine) throw 'engine should not be null.';
+        scene = createScene();};
+        initFunction().then(() => {sceneToRender = scene        
+            engine.runRenderLoop(function () {
+                if (sceneToRender && sceneToRender.activeCamera) {
+                    sceneToRender.render();
+                }
+            });
+        });
+
+        // Resize
+        window.addEventListener("resize", function () {
+            engine.resize();
+        });
